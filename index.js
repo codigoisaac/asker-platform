07e389cf7709d552939a75b6d0b0058a854ef1c2
Isaac Muniz <isaacmuniz@protonmail.com>
2022-04-24 04:44:19 -0300

    EJS setup - rendering HTML

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const ex = express()
 
+ex.set('view engine', 'ejs')
+
 ex.get('/', (req, res) => {
-  res.send('Welcome to Asker')
+  res.render('index')
 })
 
 ex.listen(10, () => {
